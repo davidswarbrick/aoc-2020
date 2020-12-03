e197072f17d2dfcf5d38e9959a01372c37c77499
David Swarbrick <david.swarbrick9@gmail.com>
2020-12-03 18:07:48 +0000

    Day 3: Neat struct dude

--- src/day03.rs
+++ src/day03.rs
@@ -1,20 +1,25 @@
 use std::fs;
-use std::ops::Rem;
 
 struct Map {
     _map : Vec<Vec<char>>,
 }
 impl Map {
-    fn get_loc(&self, pos: (usize,usize)) -> &char {
+    fn get_loc(&self, pos: (usize,usize)) -> Option<&char> {
         let (x,y) = pos;
-        self._map.get(x).unwrap().get(y).unwrap()
+        if self._map.get(y) == None{
+            return None
+        } else {
+            match self._map.get(y).unwrap().get(x){
+                Some(x) => Some(x),
+                None => self._map.get(y).unwrap().get(x % self._map.get(y).unwrap().len())
+            }
+        }
     }
-    fn is_tree(&self, pos: (usize,usize)) -> bool {
-        let c = self.get_loc(pos);
-        match c {
-            '.' => false,
-            '#' => true,
-            _ => false,
+    fn is_tree(&self, pos: (usize,usize)) -> Option<bool>{
+        match self.get_loc(pos){
+            Some('.') => Some(false),
+            Some('#') => Some(true),
+            _ => None,
         }
   
     }
@@ -32,61 +37,31 @@ fn input_from_file() -> Map {
     let map = Map {_map: vec};
     map
 }
+
+
+fn traverse_map(map: &Map, dir: (usize,usize), start_pos: (usize,usize)) -> i128 {
+    let (mut x, mut y) = start_pos;
+    let (dir_x, dir_y) = dir;
+    let mut tree_count = 0;
+    while map.is_tree((x,y)) != None {
+        if map.is_tree((x,y)).unwrap(){
+            tree_count += 1;
+        }
+        x += dir_x;
+        y += dir_y;
+    }
+    tree_count 
+}
+
+
+
 pub fn run() {
     let map = input_from_file();
-    println!("{}",map.is_tree((0,0)));
+    let slopes = vec![(1,1),(3,1),(5,1),(7,1),(1,2)];
+    let mut total: i128 = 1;
+    for slope in slopes {
+        let trees = traverse_map(&map, slope, (0,0));
+        total = total*trees;
+    }
+    println!("{}",total);
 }
-//fn input_from_file() -> Vec<Vec<char>> {
-//    let filename = "./inputs/day03.txt";
-//    let contents = fs::read_to_string(filename).expect("No input.txt present");
-//    let mut vec = Vec::new();
-//    for line in contents.lines() {
-//        let line_vec : Vec<char> = line.chars().collect();
-//        vec.push(line_vec);
-//    }
-//    vec
-//}
-//
-//fn is_tree(map: &Vec<&Vec<char>>, pos: (usize,usize)) -> bool {
-//    let (x,y) = pos;
-//    if map.get(y) == None {
-//        return false
-//    } else {
-//        let row = map.get(y).unwrap();
-//        let mut c = 'N';
-//        if row.get(x) == None{
-//            let rem = x % row.len();
-//            c = *row.get(rem).unwrap();
-//        } else {
-//            c = *row.get(x).unwrap();
-//        }
-//        match c {
-//            '.' => false,
-//            '#' => true,
-//            _ => false,
-//        }
-//    }
-//}
-//
-//
-//fn traverse_map(map: Vec<Vec<char>>, dir: (usize,usize), start_pos: (usize,usize)) -> i32 {
-//    let (mut x, mut y) = start_pos;
-//    let (dir_x, dir_y) = dir;
-//    let mut tree_count = 0;
-//    while map.get(y) != None {
-//        if is_tree(&map,(x,y)){
-//            tree_count += 1;
-//        }
-//        x += dir_x;
-//        y += dir_y;
-//    }
-//    tree_count 
-//}
-//
-//
-//
-//pub fn run() {
-//    let map = input_from_file();
-//    let trees = traverse_map(map, (3,1), (0,0));
-//    println!("Found {} trees in total",trees);
-//}
