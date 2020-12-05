ba3a3c5be04468f246f660af057b5ba1f4be7c32
David Swarbrick <david.swarbrick9@gmail.com>
2020-12-05 15:23:45 +0000

    Day 5: Finished part two, with a nasty brute force approach

--- src/day05.rs
+++ src/day05.rs
@@ -1,52 +1,49 @@
 use std::fs;
 
-fn parse_boarding_pass(bp: &str) -> i64{
+fn parse_boarding_pass(bp: &str) -> i64 {
     let mut row = 127;
     let mut col = 7;
     let mut mask = 0x40;
-    let mut cmask = 0x40 << 3; 
+    let mut cmask = 0x40 << 3;
     for c in bp.chars() {
-        //println!("{} row: {} mask: {}, col: {}, cmask: {}",c,row,mask,col,cmask);
         row = match c {
             'F' => row ^ mask,
             'B' => row | mask,
-            _ => row
+            _ => row,
         };
-        //mask = match c { 
-        //    'F' => mask >> 1,
-        //    'B' => mask >> 1,
-        //    _ => mask
-        //};
-
         col = match c {
             'R' => col | cmask,
             'L' => col ^ cmask,
-            _ => col
+            _ => col,
         };
-        
         mask >>= 1;
-        cmask >>=1;
-        //cmask = match c { 
-        //    'R' => cmask >> 1,
-        //    'L' => cmask >> 1,
-        //    _ => mask
-        //};
-        //println!("{} row: {} , col: {}",c,row,col);
+        cmask >>= 1;
     }
-    row*8 + col
+    (row << 3) + col
 }
-
-
 
 pub fn run() {
     let filename = "./inputs/day05.txt";
     let contents = fs::read_to_string(filename).expect("No input.txt present");
     let mut max_id = 0;
-    for line in contents.lines(){
+    let mut all_ids = Vec::new();
+    for line in contents.lines() {
         let seat_id = parse_boarding_pass(&line);
+        all_ids.push((seat_id >> 3, seat_id & 0x007));
         if seat_id > max_id {
             max_id = seat_id;
         }
     }
-    println!("Maximum Seat ID: {}",max_id);
+    // Not the neatest way around it, but this does a quick and dirty job of 
+    // checking if a seat is in the found boarding IDs or not.
+    // Minimum seat was 35, max was 885, so we know it's in row 4-110
+    // There must be a nicer way using the fact that the ID is [row][col] (in binary)
+    for x in 4..111 {
+        for y in 0..8 {
+            if !all_ids.contains(&(x, y)) {
+                println!("Found Empty ID {}", (x << 3) + y);
+            }
+        }
+    }
+    println!("Maximum Seat ID: {}", max_id);
 }
