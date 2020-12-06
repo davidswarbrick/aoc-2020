7d6e37f42100887de67d028b1a2f1d0f61d355a4
David Swarbrick <david.swarbrick9@gmail.com>
2020-12-06 11:48:11 +0000

    Day 6: Complete

--- src/day06.rs
+++ src/day06.rs
@@ -1,7 +1,28 @@
 use std::fs;
 
+fn everyone_yes(input: &str) -> usize{
+    let mut lines = input.lines();
+    
+    
+    let mut everyone_yes = String::from(lines.next().unwrap());
+    let mut everyone_yes_candidate = String::new(); 
+    for other_person in lines {
+        for c in other_person.chars(){
+            if everyone_yes.contains(c){
+                // If other person answered yes
+                // then add this from "yes"s
+                everyone_yes_candidate.push(c)
+            }
+        }
+        everyone_yes = everyone_yes_candidate.clone();
+        everyone_yes_candidate = String::new();
+    }
+    everyone_yes.len()
 
-fn single_group_count(input: &str) -> usize{
+}
+
+
+fn anyone_yes(input: &str) -> usize{
     let mut chars_seen = String::new();
     for c in input.chars(){
         if !chars_seen.contains(c){
@@ -21,10 +42,13 @@ fn input_from_file() -> usize{
     for line in contents.lines() {
         // Need to handle the last line, or just add another empty line
         if line.is_empty() {
-            count += single_group_count(&group);
+            //count += anyone_yes(&group);
+            println!("Group: {}",group);
+            count += everyone_yes(&group);
             group = String::new();
         } else {
             group.push_str(line);
+            group.push_str("\n");
         }
     }
     count
