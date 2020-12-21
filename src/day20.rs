eb79b6b7485c23d65f00c49366a339958c11d238
David Swarbrick <david.swarbrick9@gmail.com>
2020-12-21 14:21:57 +0000

    Day 20: immutable list of IDs, implement string reversal

--- src/day20.rs
+++ src/day20.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::fs;
-
 struct Tile {
     id: i32,
     img: String,
@@ -8,6 +7,7 @@ struct Tile {
     flip_y: bool,
     rotation: usize,
     matching_ids: [i32; 4],
+    dims: (usize,usize),
 }
 
 impl Tile {
@@ -19,31 +19,35 @@ impl Tile {
             flip_y: false,
             rotation: 0,
             matching_ids: [0, 0, 0, 0],
+            dims : (0,0),
+        }
+
+    }
+    fn set_dims(&mut self) -> (usize, usize) {
+        let x = self.img.lines().next().unwrap().len();
+        self.dims = (x,x.clone());
+        self.dims
+    }
+
+    fn get_dims(&mut self) -> (usize,usize) {
+        return match self.dims {
+            (0,0) => self.set_dims(),
+            x => x
         }
     }
     fn print(&self) {
-        println!("ID: {}", self.id);
-        println!("{}", self.img);
+        println!("ID: {} x: {} y: {} rotation: {}", self.id,self.flip_x,self.flip_y,self.rotation);
+        //println!("{}", self.img);
     }
-    fn get_column(&self, mut n: usize) -> Option<String> {
+    fn get_column(&mut self, mut n: usize) -> Option<String> {
         let mut s = String::new();
         if self.flip_x {
-            n = self.get_row(0).unwrap().len() - (n + 1);
+            let (row_len, _) = self.get_dims();
+            n = row_len - (n + 1);
         }
-
-        //let mut it = self.img.lines();
-
-        // ToDo: Implement something like this to decide iterator direction
-
-        //if self.flip_y {
-        //    it = self.img.lines().rev();
-        //}
-
-        // A match statement would be neatest, but these are diff. types.
-
-        let mut it = match self.flip_y {
-            false => self.img.lines(),
-            true => self.img.lines().rev(),
+        let it: Vec<&str> = match self.flip_y {
+            false => self.img.lines().collect(),
+            true => self.img.lines().rev().collect(),
         };
         for l in it {
             let c = l.get(n..n + 1);
@@ -55,22 +59,22 @@ impl Tile {
         }
         Some(s)
     }
-    fn get_row(&self, mut n: usize) -> Option<String> {
+    fn get_row(&mut self, mut n: usize) -> Option<String> {
         if self.flip_y {
-            n = self.get_column(0).unwrap().len() - (n + 1);
+            let (_, col_height) = self.get_dims();
+            n = col_height - (n + 1);
         }
         // ToDo: Reverse strings if needed
-        match self.img.lines().nth(n) {
-            Some(x) => Some(x.to_string()),
-            None => None,
+        let row_string = match self.img.lines().nth(n) {
+            Some(x) => x.to_string(),
+            None => return None,
+        };
+        match self.flip_x {
+            false => Some(row_string),
+            true => Some(row_string.chars().rev().collect::<String>())
         }
     }
-    fn get_dims(&self) -> (usize, usize) {
-        let x = self.get_row(0).unwrap().len();
-        let y = self.get_column(0).unwrap().len();
-        (x, y)
-    }
-    fn get_edges(&self) -> [String; 4] {
+    fn get_edges(&mut self) -> [String; 4] {
         let (x, y) = self.get_dims();
         let mut edges: [String; 4] = Default::default();
         edges[(self.rotation) % 4] = self.get_row(0).unwrap();
@@ -81,16 +85,34 @@ impl Tile {
     }
 }
 
-fn update_tile_map(mut tiles: HashMap<i32, Tile>, mut new_tile: Tile) -> HashMap<i32, Tile> {
-    tiles.remove(&new_tile.id);
-    for (_, tile) in tiles.iter_mut() {
+fn update_tile_map(mut tiles: HashMap<i32, Tile>, tile_id : &i32) -> HashMap<i32, Tile> {
+    let mut new_tile = tiles.remove(tile_id).unwrap();
+    'all_tiles: for tile in tiles.values_mut() {
         let edges = tile.get_edges();
-        let new_edges = new_tile.get_edges();
-        for i in 0..3 {
-            if new_edges[i] == edges[(i + 2) % 4] {
-                // matching edges (next to each other)
-                new_tile.matching_ids[i] = tile.id;
-                tile.matching_ids[(i + 2) % 4] = new_tile.id;
+
+        for x_flip in 0 ..=1 { 
+            new_tile.flip_x = x_flip ==1 ;
+
+            for y_flip in 0 ..=1 { 
+                new_tile.flip_y = y_flip == 1;
+
+                for rotation in 0..=3 {
+                    new_tile.rotation = rotation; 
+                    // new_tile.print();
+                    let new_edges = new_tile.get_edges();
+
+                    // For this configuration of edges, check all for match.
+                    for i in 0..3 {
+                        if new_edges[i] == edges[(i + 2) % 4] {
+                            // Record matching edges (next to each other)
+                            new_tile.matching_ids[i] = tile.id;
+                            tile.matching_ids[(i + 2) % 4] = new_tile.id;
+                            
+                            // As a match has been found, stop altering this tile
+                            break 'all_tiles;
+                        }
+                    }
+                }
             }
         }
     }
@@ -98,33 +120,31 @@ fn update_tile_map(mut tiles: HashMap<i32, Tile>, mut new_tile: Tile) -> HashMap
     tiles
 }
 
-fn input_from_file() -> HashMap<i32, Tile> {
+fn input_from_file() -> (HashMap<i32, Tile>, Vec<i32>) {
     let filename = "./inputs/day20.txt";
     let contents = fs::read_to_string(filename).expect("No input.txt present");
     let mut one_tile = String::new();
-    let mut tiles = HashMap::new();
+    let mut tile_ids = Vec::new();
+    let mut tiles : HashMap<i32, Tile>  = HashMap::new();
 
     for line in contents.lines() {
         if line.is_empty() {
             let t = Tile::new(&one_tile);
-            tiles = update_tile_map(tiles, t);
+            tile_ids.push(t.id);
+            tiles.insert(t.id,t);
             one_tile = String::new();
         } else {
             one_tile.push_str(line);
             one_tile.push_str("\n");
         }
     }
-    tiles
+    (tiles,tile_ids)
 }
 
 pub fn run() {
-    let mut tiles = input_from_file();
-    // ToDo : Re-architect this to be memory safe. (Compilation fails)
-    for tile in tiles.values_mut() {
-        //println!("{} : {:?}", tile.id, tile.matching_ids);
-        if tile.matching_ids == [0, 0, 0, 0] {
-            tile.rotation += 1;
-            tiles = update_tile_map(tiles, *tile);
-        }
+    let (mut tiles, tile_ids) = input_from_file();
+    for id in tile_ids {
+        tiles = update_tile_map(tiles,&id);
+        println!("{} : {:?}",id,tiles[&id].matching_ids);
     }
 }
